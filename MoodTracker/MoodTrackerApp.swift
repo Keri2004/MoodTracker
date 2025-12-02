1b223c9fb6b41718c2e61fbc3c60a358f6bc9f24
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2025-12-02 04:13:29 -0600

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Керемет  on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
